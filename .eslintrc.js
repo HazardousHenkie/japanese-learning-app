21ec1970116c4926a17b9b95c25dde56ebb82e79
HazardousHenkie <thekyle@hotmail.com>
2021-01-24 17:21:54 +0900

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
-  },
-  overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
-};
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/recommended',
+        'eslint:recommended',
+        '@vue/typescript/recommended',
+        '@vue/prettier',
+        '@vue/prettier/@typescript-eslint',
+        '@vue/typescript/recommended',
+        'plugin:vue-a11y/base'
+    ],
+    plugins: ['vue-a11y'],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        "@typescript-eslint/no-explicit-any": "warn",
+        'no-console': 'warn',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        "vue/no-multiple-template-root": "off",
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
+}
