10e833b92b7cd58dfdd212a59841875fd9861b3a
HazardousHenkie <thekyle@hotmail.com>
2021-01-30 22:40:32 +0900

    adding tailwind csss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,25 @@
 <template>
-    <div id="nav">
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-    </div>
+    <nav class="bg-gray-800">
+        <div class="mx-auto px-2 sm:px-6 lg:px-8">
+            <div class="relative flex items-center justify-between h-16">
+                <div class="hidden sm:block sm:ml-6">
+                    <div class="flex space-x-4">
+                        <router-link
+                            class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            to="/"
+                            >Home</router-link
+                        >
+                        |
+                        <router-link
+                            class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                            to="/about"
+                            >About</router-link
+                        >
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     <router-view />
 </template>
-
-<style>
-#app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-}
-
-#nav a {
-    font-weight: bold;
-    color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-    color: #42b983;
-}
-</style>
