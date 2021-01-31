2b26a58149d4736bc5499e6e2daf980229af653c
HazardousHenkie <thekyle@hotmail.com>
2021-01-31 22:09:05 +0900

    initial

---
+++ src/components/atoms/Map.vue
@@ -0,0 +1,57 @@
+<template>
+    <div ref="mapDivRef" class="map"></div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue'
+
+export default {
+    name: 'GoogleMaps',
+    props: {
+        lat: { type: Number, default: 38.0 },
+        lng: { type: Number, default: -77.01 },
+
+        zoom: { type: Number, default: 18 },
+        mapType: { type: String, default: 'hybrid' },
+        disableUI: { type: Boolean, default: false }
+    },
+    setup(props) {
+        const map = ref(null)
+        const mapDivRef = ref(null)
+
+        onMounted(() => {
+            const key = process.env.VUE_APP_GOOGLEMAPS_KEY
+
+            // create the script element to load
+            const googleMapScript = document.createElement('SCRIPT')
+            googleMapScript.setAttribute(
+                'src',
+                `https://maps.googleapis.com/maps/api/js?key=${key}&callback=initMap`
+            )
+            googleMapScript.setAttribute('defer', '')
+            googleMapScript.setAttribute('async', '')
+            document.head.appendChild(googleMapScript)
+        })
+
+        window.initMap = () => {
+            map.value = new window.google.maps.Map(mapDivRef.value, {
+                mapTypeId: props.mapType,
+                zoom: props.zoom,
+                disableDefaultUI: props.disableUI,
+                center: { lat: props.lat, lng: props.lng }
+            })
+        }
+
+        return {
+            mapDivRef
+        }
+    }
+}
+</script>
+
+<style lang="css" scoped>
+.map {
+    width: 100%;
+    height: 100vh;
+}
+</style>
